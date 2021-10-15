59d4e62b190601f9cd90cdc571ed2f621d81d0bc
gozeon <goze.qiu@gmail.com>
2021-10-15 15:59:19 +0800

    chore: 区分环境

--- vue.config.js
+++ vue.config.js
@@ -1,12 +1,13 @@
 module.exports = {
-    devServer: {
-      proxy: {
-        '^/api': {
-            target: 'http://localhost:5000',
-            changeOrigin: true,
-            ws: true,
-            pathRewrite: {"^/api" : ""}
-        }
-      }
-    }
-  }
+  publicPath: process.env.NODE_ENV === 'production' ? '/static/sanmi' : '/',
+  devServer: {
+    proxy: {
+      '^/api': {
+        target: 'http://localhost:5000',
+        changeOrigin: true,
+        ws: true,
+        pathRewrite: { '^/api': '' },
+      },
+    },
+  },
+}
